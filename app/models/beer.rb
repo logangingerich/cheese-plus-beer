841b51767a96379d0d81fa4fd26fb7fa50cccbb8
Logan Gingerich <logangingerich@yahoo.com>
2017-03-25 11:08:26 -0600

    Changed search to be non-case-sensitive

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,6 @@
 class Beer < ActiveRecord::Base
   has_and_belongs_to_many :cheese
   def self.search(search)
-    where("name LIKE ?", "%#{search}%")
+    where("LOWER(name) LIKE ?", "%#{search}%")
   end
 end
